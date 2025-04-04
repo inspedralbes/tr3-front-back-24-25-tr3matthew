570826441a1f670b14efade37ee67ce30c4c996f
a23matcasmar <a23matcasmar@inspedralbes.cat>
2025-04-04 12:08:21 +0200

    cambios

--- vuetify/src/components/Login.vue
+++ vuetify/src/components/Login.vue
@@ -37,25 +37,39 @@ const vContraseña = ref([
 </script>
 
 <template>
-    <v-form ref="form">
-        <v-text-field
-        v-model="usuario"
-        :rules="vUsuario"
-        label = "Usuario">
-        </v-text-field>
-        <v-text-field
-        v-model="contraseña"
-        type="password"
-        :rules="vContraseña"
-        label =  "Contraseña">
-        </v-text-field>
-        <v-btn
-        @click="getUnicaCuenta">
+  <div class="login-background">
+    <v-container class="fill-height d-flex justify-center align-center">
+      <v-card class="pa-8" width="400" elevation="10">
+        <v-form ref="form">
+          <v-text-field
+            v-model="usuario"
+            :rules="vUsuario"
+            label="Usuario"
+          ></v-text-field>
+          <v-text-field
+            v-model="contraseña"
+            type="password"
+            :rules="vContraseña"
+            label="Contraseña"
+          ></v-text-field>
+          <v-btn
+            class="mt-4"
+            color="primary"
+            block
+            @click="getUnicaCuenta"
+          >
             Ingresar
-        </v-btn>
-    </v-form>
+          </v-btn>
+        </v-form>
+      </v-card>
+    </v-container>
+  </div>
 </template>
 
 <style scoped>
-
+.login-background {
+  background-color: #E6E6FA; /* Morado pastel */
+  height: 100vh;
+  width: 100%;
+}
 </style>
